21efb85ad8a9416587bb67520d61c41f87d8c5c5
Shreyash Sutar <130670400+ShreyashSutar@users.noreply.github.com>
2023-04-18 17:53:25 +0530

    Add files via upload

---
+++ Photography Website/JS/main.js
@@ -0,0 +1,27 @@
+
+let index = 0;
+const totalWorkItems = $(".work-item").length;
+
+$(document).ready(function () {
+
+    // nav toggle
+    $(".nav-toggle").click(function(){
+        $(".header .nav").slideToggle();
+    })
+    $(".header .nav a").click(function(){
+        if($(window).width() < 768){
+            $(".header .nav").slideToggle();
+        }
+    })
+
+    //fixed header
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $(".header").addClass("fixed");
+        }
+        else{
+            $(".header").removeClass("fixed");
+        }
+    })
+})
+
